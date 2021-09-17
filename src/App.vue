d65470f19b936736bb2882f5306d05da8581bf68
spata00 <sararares12@gmail.com>
2021-09-17 13:41:18 +0200

    fatto con bonus

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hero/>
+    <Title/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from './components/Hero.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hero,
+    Title
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="jumbo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .jumbo {
+        content: "";
+        height:100px;
+        width:100%;
+        background-color:rebeccapurple;
+    }
+</style>
